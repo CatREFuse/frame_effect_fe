3ac46409d21b5cd2f1ddbeaec6e800f5aa542209
tanshow <anlocetic@foxmail.com>
2021-05-29 00:00:42 +0800

    完成基础交互

---
+++ src/assets/coordinate.py
@@ -0,0 +1,17 @@
+import csv
+from pprint import pprint
+import json
+
+with open('./coordinate.csv', 'r') as f:
+    reader = csv.reader(f)
+    arr = []
+    for row in reader:
+        dict = {}
+        dict['start'] = [row[0], row[1]]
+        dict['target'] = [row[2], row[3]]
+        arr.append(dict)
+    arr.pop(0)
+    pprint(arr)
+    json_str = json.dumps(arr)
+    with open('./coordinate.json', 'w+') as jn:
+        jn.write(json_str)
